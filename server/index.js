ec0369c0a010628af04afc98c489d29509a68be2
ranjit95s <ranjitsuryawanshi952@gmail.com>
2024-06-25 12:55:33 +0530

    Update ESLint configuration and dependencies

--- server/index.js
+++ server/index.js
@@ -14,8 +14,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = 'mongodb://localhost:27017/kaustubhMemoriesProject';
+const PORT = process.env.PORT || 5000;
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
